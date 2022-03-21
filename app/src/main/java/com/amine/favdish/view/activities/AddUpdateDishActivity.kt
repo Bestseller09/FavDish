e726de0a1269fc683d97e59fffc0828ae0f1e585
Bestseller09 <aoudjehane@gmail.com>
2022-03-21 00:53:08 -0400

    favdish

---
+++ app/src/main/java/com/amine/favdish/view/activities/AddUpdateDishActivity.kt
@@ -0,0 +1,12 @@
+package com.amine.favdish.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amine.favdish.R
+
+class AddUpdateDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_dish)
+    }
+}
